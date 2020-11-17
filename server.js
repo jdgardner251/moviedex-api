2f88bbccd87195d0bbc891f563f1073a943620af
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-11-17 16:58:02 -0600

    work done

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
+const MOVIEDEX = require('./moviedex.json')
+
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello Express");
-});
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    
+    if (!authToken || authToken.split(" ")[1] !== apiToken) {
+        return res.status(401).json({ error: "Unauthorized request" });
+      }
+    // move to the next middleware
+    next()
+})
+
+function handleGetMovie(req, res) {
+    res.send(MOVIEDEX)
+}
+
+app.get('/movie', handleGetMovie)
 
 app.listen(8000, () => {
   console.log("server listening on 8000");
