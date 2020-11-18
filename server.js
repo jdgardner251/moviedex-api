54d5c21528338bb14c7f5b6b49e310bf5891dff9
JD Gardner <jdgardner@aptuscapitaladvisors.com>
2020-11-18 15:31:56 -0600

    working assignment

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require('helmet')
+const cors = require('cors')
 const MOVIEDEX = require('./moviedex.json')
 
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(helmet())
+app.use(cors())
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
@@ -19,11 +23,30 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
-function handleGetMovie(req, res) {
-    res.send(MOVIEDEX)
-}
+app.get('/movie', (req, res) => {
+    const { genre = "", country = "", avg_vote = "" } = req.query;
+    let response = MOVIEDEX;
 
-app.get('/movie', handleGetMovie)
+    // filter movies by genre if genre query param is present
+    if (genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(genre.toLocaleLowerCase()))
+    }
+
+    // filter movies by country if country query param is present
+    if (country) {
+        response = response.filter(country =>
+            movie.country.toLocaleLowerCase().includes(country.toLocaleLowerCase()) )
+    }
+
+    if (avg_vote) {
+        const voteNum = parseFloat(avg_vote);
+        response = response.filter(movie => movie.avg_vote >= voteNum)
+    }
+
+   res.json(response)
+});
+
 
 app.listen(8000, () => {
   console.log("server listening on 8000");
